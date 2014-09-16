e4e8c890143dd1dcb15fce78f2885895cd27cc08
galicians <galicians@gmail.com>
2014-09-16 14:33:29 +0100

    dock station after testing capacity

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,14 +1,25 @@
 require 'docking_station'
 
 describe DockingStation do
+	let(:bike) { Bike.new }
+	let(:station) { DockingStation.new(:capacity => 20) } 
 
 	it "dock station should accept a bike" do
-		bike = Bike.new
-		station = DockingStation.new
 		expect(station.bike_count).to eq(0)
 		station.dock(bike)
 		expect(station.bike_count).to eq(1)		
 	end
 
+	it "should release a bike" do
+		station.dock(bike)
+		station.release(bike)
+		expect(station.bike_count).to eq(0)
+	end
+
+	it "should have a finite capacity" do
+		expect(station).not_to be_full
+		20.times { station.dock(Bike.new) }
+		expect(station).to be_full
+	end	
 	
 end
